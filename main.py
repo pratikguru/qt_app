c5a414d4b44b5ada12b1ba5552013674f6b1a302
prateek <pratik@cthings.co>
2019-03-19 23:42:04 +0100

    New features added

--- main.py
+++ main.py
@@ -1,21 +1,93 @@
 import time
 import sys
-from   PyQt5.QtCore    import pyqtSlot
-from   PyQt5           import QtWidgets, QtGui
-from   PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
-from   PyQt5.QtWidgets import *
-from   PyQt5.uic       import loadUi
+try:
+    from   PyQt5             import QtCore
+    from   PyQt5.QtCore    import pyqtSlot
+    from   PyQt5           import QtWidgets, QtGui
+    from   PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
+    from   PyQt5.QtWidgets import *
+    from   PyQt5.uic       import loadUi
+except Exception as e:
+    print ("Requirement Error! The PyQt Libs are not installed.")
+    import os
 
-class Example(QWidget):
 
-    def __init__(self):
-        super().__init__()
+
+class BookDialog():
+    def __init__(self, string, btnString):
+        self.miniDialog = QDialog()
+        self.miniDialog.setGeometry( 500, 500, 400, 300 )
+        self.miniDialog.setWindowTitle(str(string))
+        self.addButton    = QPushButton(str(btnString), self.miniDialog)
+        self.cancelButton = QPushButton("Cancel", self.miniDialog)
+        self.groupBox     = QGroupBox("Add details", self.miniDialog)
+        self.author_label = QLabel("Author: ", self.groupBox)
+        self.author_edit  = QLineEdit(self.groupBox)
+
+        self.title_label    = QLabel("Title: ", self.groupBox)
+        self.title_lineEdit = QLineEdit(self.groupBox)
+
+        self.year_label    = QLabel("Year: ", self.groupBox)
+        self.year_lineEdit = QLineEdit(self.groupBox)
+
+        self.cancelled = False
+        self.isMax99999 = QtGui.QRegExpValidator(QtCore.QRegExp("^[0-9]{1,5}$"))
+        self.year_lineEdit.setValidator(self.isMax99999)
         self.initUI()
 
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 220)
+        #Setting up geometry
+        self.groupBox.setGeometry(10, 10, 381, 211)
+        self.author_edit.setGeometry(100, 20, 251, 31)
+        self.author_label.setGeometry(20, 30, 61, 16)
+        self.title_label.setGeometry(20, 80, 61, 16)
+        self.title_lineEdit.setGeometry(100, 70, 251, 31)
+        self.year_label.setGeometry(20, 120, 61, 31)
+        self.year_lineEdit.setGeometry(100, 120, 251, 31)
+        self.addButton.setGeometry(20, 240, 121, 51)
+        self.cancelButton.setGeometry(160, 240, 121, 51)
 
+        #Fonts and customization
+        self.font = QtGui.QFont()
+        self.input_font = QtGui.QFont("Ariel")
+        self.input_font.setPointSize(13)
+        self.font.setBold(True)
+
+        self.author_label.setFont(self.font)
+        self.title_label.setFont(self.font)
+        self.year_label.setFont(self.font)
+
+        self.author_edit.setFont(self.input_font)
+        self.title_lineEdit.setFont(self.input_font)
+        self.year_lineEdit.setFont(self.input_font)
+
+        self.author_edit.setStyleSheet(""" border-radius: 10px; """)
+        self.title_lineEdit.setStyleSheet(""" border-radius:10px;""")
+        self.year_lineEdit.setStyleSheet(""" border-radius:10px;""")
+
+    def run(self):
+        self.addButton.clicked.connect(self.addBookEmitSignal)
+        self.cancelButton.clicked.connect(self.cancelBookEmitSignal)
+        self.miniDialog.exec_()
+
+    def addBookEmitSignal(self):
+        safeToQuit = True
+        if not self.author_edit.text():
+            print ("Please add Author.")
+            safeToQuit = False
+        if not self.title_lineEdit.text():
+            print ("Please add Title.")
+            safeToQuit = False
+        if not self.year_lineEdit.text():
+            print ("Please add a Year.")
+            safeToQuit = False
+        if safeToQuit:
+            self.miniDialog.close()
+
+    def cancelBookEmitSignal(self):
+         self.cancelled = True
+         self.miniDialog.close()
 
 
 class App(QMainWindow):
@@ -31,7 +103,8 @@ class App(QMainWindow):
         self.selectedColumn = 0
         self.pageCount      = 0
         self.currentPage    = 0
-        self.exm            = Example()
+        self.addDialog      = BookDialog("Add Book", "Add")
+
      def run(self):
          self.makeTable()
          self.add_button.clicked.connect(self.addBook)
@@ -50,54 +123,21 @@ class App(QMainWindow):
         for x in range(0, 3):
             self.tableWidget.setColumnWidth(x, 315)
 
+     def generateDialog(self, type):
+                 self.addDialog.run()
+
      def addBook(self):
-        miniDialog  = QDialog()
-        miniDialog.setGeometry(500, 500, 400, 300)
-        miniDialog.setWindowTitle("Add Book")
-        addButton = QPushButton("Add", miniDialog)
-        cancelButton =QPushButton("Cancel", miniDialog)
-        groupBox   = QGroupBox("Add details", miniDialog)
-        author_label = QLabel("Author: ", groupBox)
-        author_edit  = QLineEdit(groupBox)
-
-        title_label = QLabel("Title: ", groupBox)
-        title_lineEdit = QLineEdit(groupBox)
-
-        year_label = QLabel("Year: ", groupBox)
-        year_lineEdit = QLineEdit(groupBox)
-
-        groupBox.setGeometry(10, 10, 381, 211)
-        author_edit.setGeometry(100, 20, 251, 31)
-        author_label.setGeometry(20, 30, 61, 16)
-        title_label.setGeometry(20, 80, 61, 16)
-        title_lineEdit.setGeometry(100, 70, 251, 31)
-        year_label.setGeometry(20, 120, 61, 31)
-        year_lineEdit.setGeometry(100, 120, 251, 31)
-        addButton.setGeometry(20, 240, 121, 51)
-        cancelButton.setGeometry(160, 240, 121, 51)
-
-        font = QtGui.QFont()
-        input_font = QtGui.QFont("Ariel")
-        input_font.setPointSize(13)
-        font.setBold(True)
-
-        author_label.setFont(font)
-        title_label.setFont(font)
-        year_label.setFont(font)
-
-        author_edit.setFont(input_font)
-        title_lineEdit.setFont(input_font)
-        year_lineEdit.setFont(input_font)
-
-        author_edit.setStyleSheet(""" border-radius: 10px; """)
-        title_lineEdit.setStyleSheet(""" border-radius:10px;""")
-        year_lineEdit.setStyleSheet(""" border-radius:10px;""")
-
-        
-        miniDialog.exec_()
-        print ("Add Book has been called.")
-
-        book = ["one", "two", "three"]
+        self.generateDialog("Add")
+        if self.addDialog.cancelled:
+            self.addDialog.cancelled = False
+            return
+        if not (self.addDialog.author_edit.text() or self.addDialog.title_lineEdit.text() or self.addDialog.year_lineEdit.text()):
+            return
+        book = [
+                 str( self.addDialog.author_edit.text()   ),
+                 str( self.addDialog.title_lineEdit.text() ),
+                 str( self.addDialog.year_lineEdit.text() )
+                ]
         rowCount = self.tableWidget.rowCount()
         self.tableWidget.insertRow(rowCount)
         self.tableWidget.setRowHeight(rowCount, 75)
